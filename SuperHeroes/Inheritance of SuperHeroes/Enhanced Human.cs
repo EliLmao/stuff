73ee7f958192319eec8a3b727ea01ff2ad76b434
EliLmao <50881421+EliLmao@users.noreply.github.com>
2019-05-22 23:04:24 +1000

    Add files via upload

---
+++ SuperHeroes/Inheritance of SuperHeroes/Enhanced Human.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_of_SuperHeroes
+{
+    class Enhanced_Human : SuperHero
+    {
+        int sumOfPowers;
+        bool enhanced = false;
+        private List<SuperPower> powerSet;
+
+        public Enhanced_Human(string name, string secretId, List<SuperPower> myPowers)
+        {
+            foreach (int power in powerSet)
+            {
+                sumOfPowers++;
+            }
+
+            base.SwitchIdentity();
+
+            
+        }
+
+        public SwitchIdentity()
+        {
+
+        }
+
+        public override bool HasPower(SuperPower whatPower)
+        {
+            if ((enhanced == true) & (powerSet.Contains(whatPower)))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int TotalPower()
+        {
+
+        }
+
+    }
+}
---
+++ SuperHeroes/Inheritance of SuperHeroes/Super Human.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_of_SuperHeroes {
+    class Super_Human : SuperHero {
+
+        private int sumOfPowers;
+        private List<SuperPower> powerSet;
+        private List<SuperPower> originalPowers;
+        private int originalSumOfPowers;
+
+        public Super_Human(string name, string secretId, List<SuperPower> myPowers)
+            : base(name, secretId) {
+            sumOfPowers = 0;
+            powerSet = myPowers;
+            originalPowers = powerSet;
+
+            foreach (SuperPower power in powerSet) {
+                sumOfPowers += (int)power;
+            }
+            originalSumOfPowers = sumOfPowers;
+        }
+
+        public override bool HasPower(SuperPower whatPower) {
+            return powerSet.Contains(whatPower);
+        }
+
+        public override int TotalPower() {
+            return sumOfPowers;
+        }
+
+        public void AddSuperPower(SuperPower newPower) {
+            if (!powerSet.Contains(newPower)) {
+                powerSet.Add(newPower);
+                sumOfPowers += (int)newPower;
+            }
+        }
+
+        public void LoseSinglePower(SuperPower power) {
+            if (powerSet.Contains(power)) {
+                powerSet.Remove(power);
+                sumOfPowers -= (int)power;
+            }
+        }
+
+        public void LoseAllSuperPowers() {
+            powerSet.Clear();
+            sumOfPowers = 0;
+        }
+
+        public void ResetPowers() {
+            powerSet = originalPowers;
+            sumOfPowers = originalSumOfPowers;
+        }
+    }
+}
