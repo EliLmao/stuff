1250977f83a7388661d2512737e4b6ebd5be4999
EliLmao <50881421+EliLmao@users.noreply.github.com>
2019-06-02 18:59:07 +1000

    Add files via upload

---
+++ updatedCAB230/src/main.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+
+export default function Main(props) {
+  return <p>You are now logged in</p>;
+  // return <p>We're at the main page your token is: {props.token}</p>;
+}
---
+++ updatedCAB230/src/search.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+// import ReactTable from "react-table";
+
+import "./styles.css";
+
+export default function SearchInput(props) {
+  const [search, setSearch] = useState("");
+
+  function SearchAttempt() {
+    //The parameters of the call
+    let getParam = { method: "GET" };
+    console.log("find me adam: ", global.JWT);
+    let head = { Authorization: `Bearer ${global.JWT}` };
+    getParam.headers = head;
+
+    //The URL
+    const baseUrl = "https://cab230.hackhouse.sh/search?";
+    const query = "offence=" + search;
+    
+    const url = baseUrl + query;
+    console.log(url);
+
+    fetch(encodeURI(url), getParam)
+      .then(function(response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(function(result) {
+        let appDiv = document.getElementById("app");
+        appDiv.innerHTML = JSON.stringify(result);
+      })
+      .catch(function(error) {
+        console.log(
+          "There has been a problem with your fetch operation: ",
+          error.message
+        );
+      });
+  }
+
+  return (
+    <div className="App">
+      <h2>Search:</h2>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          //console.log(event.target.elements.name.value);
+        }}
+      >
+        <label htmlFor="name">Search:</label>
+        <input
+          name="text"
+          id="text"
+          type="text"
+          placeholder="Enter query"
+          value={search}
+          onChange={event => {
+            setSearch(event.target.value);
+            console.log(search);
+          }}
+        />
+        <button
+          onClick={event => {
+            SearchAttempt();
+          }}
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+    
+    // <ReactTable
+    // data={this.props.offencelistfromparent.result}
+    // columns={[
+    //     {
+    //     Header: "Name",
+    //     columns: [
+    //         {
+    //             Header: "LGA",
+    //             id: "lga",
+    //             accessor: d => d.LGA
+    //         },
+    //         {
+    //             Header: "Total",
+    //             id: "total",
+    //             accessor: d => d.total
+    //         },
+    //         {
+    //             Header: "Latitude",
+    //             id: "lat",
+    //             accessor: d => d.lat
+    //         },
+    //         {
+    //             Header: "longitude",
+    //             id: "lng",
+    //             accessor: d => d.lng
+    //         }
+    //     ]
+    //     }
+    // ]}
+    
+  );
+}
